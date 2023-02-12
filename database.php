5084c9bc29d5ca03d93ec94576b7f6c9b8464919
giewendi <giewenpinlac@gmail.com>
2023-02-12 15:08:02 +0800

    Removing ignored files

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/vendor/autoload.php";
-$client = new MongoDB\Client(
-    'mongodb+srv://gdpinlac:gdpinlac@cluster0.8xokbod.mongodb.net/?retryWrites=true&w=majority');
-//Select DB
-$db = $client->dbResto;
-//Select Collection
-$collection = $db->colresto;
-?>
